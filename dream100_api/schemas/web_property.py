6f2e97259d307de3945fabfd189a262d98184318
John Davenport <johns10@gmail.com>
2024-08-16 11:10:29 -0400

    feat: create web_properties schema in dream100_api/schemas

---
+++ dream100_api/schemas/web_property.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from enum import Enum
+from typing import Optional
+
+class WebPropertyType(str, Enum):
+    FACEBOOK = "facebook"
+    YOUTUBE = "youtube"
+    LINKEDIN = "linkedin"
+    WEBSITE = "website"
+    TWITTER = "twitter"
+
+class WebPropertyBase(BaseModel):
+    influencer_id: int
+    type: WebPropertyType
+    url: str
+    followers: Optional[int] = None
+
+class WebPropertyCreate(WebPropertyBase):
+    pass
+
+class WebPropertyUpdate(BaseModel):
+    type: Optional[WebPropertyType] = None
+    url: Optional[str] = None
+    followers: Optional[int] = None
+
+class WebProperty(WebPropertyBase):
+    id: int
+
+    class Config:
+        orm_mode: True

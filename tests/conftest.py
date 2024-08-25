d46ecf763b2ed2abb467a07fab927519252ff1cc
John Davenport <johns10@gmail.com>
2024-08-25 16:03:02 -0400

    Fix deploy error

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,6 @@ from dream100.db_config import Base, get_db, init_db
 from dream100_api.main import app
 from fastapi.testclient import TestClient
 from config import config
-from dream100.services.get_influencer_web_properties import (
-    InfluencerWebPropertiesService,
-)
 from dream100.models.project import Project
 from dream100.models.influencer import Influencer
 from dream100.models.web_property import WebProperty, WebPropertyType
@@ -15,6 +12,7 @@ from dream100.models.content import Content, ContentStatus
 from dream100.models.content_embedding import ContentEmbedding
 from dream100.utilities import model as original_model
 from tests.mocks.mock_model import MockEmbeddingModel
+from tests.test_helpers import create_float_array
 
 
 @pytest.fixture(scope="function", autouse=True)
